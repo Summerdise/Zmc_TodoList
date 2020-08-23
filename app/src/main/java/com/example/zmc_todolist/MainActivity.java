5b815b819b3c3e3a00911b4e6dd64c13ecd349c1
Summerdise <350464517@qq.com>
2020-08-24 01:57:28 +0800

    [zmc] feat: add click button to show wrong tips

--- app/src/main/java/com/example/zmc_todolist/MainActivity.java
+++ app/src/main/java/com/example/zmc_todolist/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.zmc_todolist;
 
-import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -17,6 +17,10 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     ViewModel viewModel;
+    int USER_MIN_LETTER_NUMBER = 3;
+    int USER_MAX_LETTER_NUMBER = 12;
+    int PASSWORD_MIN_LETTER_NUMBER = 6;
+    int PASSWORD_MAX_LETTER_NUMBER = 18;
 
     @BindView(R.id.user_name_input)
     EditText userNameInput;
@@ -40,13 +44,36 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         userNameInput.addTextChangedListener(new LetterNumberWatcher());
         passwordInput.addTextChangedListener(new LetterNumberWatcher());
+        userNameWrongTipButton.setOnClickListener(view -> {
+            int length = userNameInput.getText().toString().length();
+            if (length < USER_MIN_LETTER_NUMBER) {
+                userNameWrongTipText.setText("用户名需要输入至少3个字符");
+                userNameWrongTipText.setVisibility(View.VISIBLE);
+            }
+            if (length > USER_MAX_LETTER_NUMBER) {
+                userNameWrongTipText.setText("用户名最多输入12个字符");
+                userNameWrongTipText.setVisibility(View.VISIBLE);
+            }
+        });
+        passwordWrongTipButton.setOnClickListener(view -> {
+            int length = passwordInput.getText().toString().length();
+            if (length < PASSWORD_MIN_LETTER_NUMBER) {
+                passwordWrongTipText.setText("用户名需要输入至少6个字符");
+                passwordWrongTipText.setVisibility(View.VISIBLE);
+            }
+            if (length > PASSWORD_MAX_LETTER_NUMBER) {
+                passwordWrongTipText.setText("用户名最多输入18个字符");
+                passwordWrongTipText.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     class LetterNumberWatcher implements TextWatcher {
 
         @Override
         public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+            userNameWrongTipText.setVisibility(View.INVISIBLE);
+            passwordWrongTipText.setVisibility(View.INVISIBLE);
         }
 
         @Override
@@ -80,9 +107,6 @@ public class MainActivity extends AppCompatActivity {
 
         public boolean judgePasswordRightly() {
             int length = passwordInput.getText().toString().length();
-            int PASSWORD_MIN_LETTER_NUMBER = 6;
-            int PASSWORD_MAX_LETTER_NUMBER = 18;
-
             if (length == 0) {
                 passwordWrongTipButton.setVisibility(View.INVISIBLE);
                 return false;
@@ -95,15 +119,48 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        @SuppressLint("UseCompatLoadingForDrawables")
         public void loginButtonChange() {
             if (judgeUserNameRightly() && judgePasswordRightly()) {
                 loginInButton.setEnabled(true);
                 loginInButton.setBackground(getResources().getDrawable(R.drawable.btn_able_background_login_in));
-            }else{
+            } else {
                 loginInButton.setEnabled(false);
                 loginInButton.setBackground(getResources().getDrawable(R.drawable.btn_unable_background_login_in));
             }
         }
     }
+
+//    class wrongNameButtonClick implements View.OnClickListener {
+//        int length = userNameInput.getText().toString().length();
+//
+//        @Override
+//        public void onClick(View view) {
+//            if (length < USER_MIN_LETTER_NUMBER) {
+//                userNameWrongTipText.setText("用户名需要输入至少3个字符");
+//                userNameWrongTipText.setVisibility(View.VISIBLE);
+//            }
+//            if (length > USER_MAX_LETTER_NUMBER) {
+//                userNameWrongTipText.setText("用户名最多输入12个字符");
+//                userNameWrongTipText.setVisibility(View.VISIBLE);
+//            }
+//        }
+//    }
+//
+//    class wrongPasswordButtonClick implements View.OnClickListener {
+//        int length = passwordInput.getText().toString().length();
+//
+//        @Override
+//        public void onClick(View view) {
+//            if (length < PASSWORD_MIN_LETTER_NUMBER) {
+//                passwordWrongTipText.setText("用户名需要输入至少6个字符");
+//                passwordWrongTipText.setVisibility(View.VISIBLE);
+//            }
+//            if (length > PASSWORD_MAX_LETTER_NUMBER) {
+//                passwordWrongTipText.setText("用户名最多输入18个字符");
+//                passwordWrongTipText.setVisibility(View.VISIBLE);
+//            }
+//        }
+//    }
 
 }
