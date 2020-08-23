b780724b88a2f163c409cedeeae2de082bfee344
Summerdise <350464517@qq.com>
2020-08-23 16:22:21 +0800

    [zmc] feat: add text watcher classes

--- app/src/main/java/com/example/zmc_todolist/MainActivity.java
+++ app/src/main/java/com/example/zmc_todolist/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.zmc_todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewModel viewModel;
+
+    @BindView(R.id.user_name_input)
+    TextView userNameInput;
+    @BindView(R.id.password_input)
+    TextView passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userNameInput.addTextChangedListener(new UserLetterNumberWatcher());
+
     }
 }
---
+++ app/src/main/java/com/example/zmc_todolist/ViewModel.java
@@ -0,0 +1,6 @@
+package com.example.zmc_todolist;
+
+public class ViewModel {
+
+
+}
