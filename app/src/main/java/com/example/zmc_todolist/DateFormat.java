28a93c78de5e0c767b0468412430b032403bb35b
Summerdise <350464517@qq.com>
2020-08-26 03:37:22 +0800

    [zmc] feat: add date after showing

--- app/src/main/java/com/example/zmc_todolist/DateFormat.java
+++ app/src/main/java/com/example/zmc_todolist/DateFormat.java
@@ -11,17 +11,29 @@ public class DateFormat {
         @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日");
         return formatter.format(date);
     }
+
     public String toChineseMonthDay(Date date) {
         @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("MM月dd日");
         return formatter.format(date);
     }
 
     public String toEnglishWeekDay(Date date) {
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("EEEE，d",Locale.ENGLISH);
-        return formatter.format(date);
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("EEEE，d", Locale.ENGLISH);
+        String format = formatter.format(date);
+        char lastChar = format.charAt(format.length()-1);
+        if (lastChar == '1') {
+            return format + "st";
+        } else if (lastChar == '2') {
+            return format + "nd";
+        } else if (lastChar == '3') {
+            return format + "rd";
+        } else {
+            return format + "th";
+        }
     }
+
     public String toEnglishMonth(Date date) {
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("MMMM",Locale.ENGLISH);
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("MMMM", Locale.ENGLISH);
         return formatter.format(date);
     }
 }
